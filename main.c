684430f7f3043e5011b36d9500e69e243bbdd495
Korbo <korbin.m.marshall@gmail.com>
2025-07-13 14:34:09 -0500

    change

--- main.c
+++ main.c
@@ -16,14 +16,14 @@ void main() {
 	clrscr();
 	VIC.spr_pos[0].x = 24;
 	VIC.spr_pos[0].y = 50;
-	VIC.spr_ena = 0b00000001;
 	VIC.spr_exp_y  = 0b00000001;
 	VIC.spr_mcolor = 0b00000001;
 	VIC.spr_mcolors[0] = COLOR_LIGHTGREEN;
 	VIC.spr_mcolors[1] = COLOR_YELLOW;
 	VIC.spr_color[0] = COLOR_BROWN;
-	*(unsigned char*)0x7F8 = SPRITE_LOCATION / 64;
 	VIC.bgcolor[0] = COLOR_WHITE;
+	VIC.spr_ena = 0b00000001;
+	*(unsigned char*)0x7F8 = SPRITE_LOCATION / 64;
 	for (;;) {
 		memcpy((unsigned*)0x3EC0, shiggy+(63*frame), shiggy_size);
 		frame++;
