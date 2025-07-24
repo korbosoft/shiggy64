175866694e560aa6d26657df9ab593ffa1da1ff8
Korbo <korbin.m.marshall@gmail.com>
2025-07-24 12:16:02 -0500

    shig counter, fix hi_x bug

--- main.c
+++ main.c
@@ -8,9 +8,12 @@
 
 #define SPRITE_LOCATION 0x3EC0
 
-int x = 172, y = 125;
+unsigned int x = 172;
+unsigned char y = 125;
 bool alreadyFired = false;
-char frame = 0;
+unsigned char frame = 0;
+char shigCount;
+bool doFrame = false;
 
 void handle_input(void) {
 	if (JOY_LEFT(joy_read(JOY_1))) {
@@ -34,25 +37,31 @@ void handle_input(void) {
 	}
 
 	if (JOY_BTN_1(joy_read(JOY_1)) && (alreadyFired == false)) {
-		frame = (frame + 1) % 5;
+		if (doFrame == true) {
+			frame = (1 + frame) % 5;
+			memcpy((unsigned char*)SPRITE_LOCATION, shiggy+(63*frame), shiggy_size);
+			if (frame == 0) ++shigCount;
+			doFrame = false;
+		} else doFrame = true;
 		alreadyFired = true;
+
 	} else if ((JOY_BTN_1(joy_read(JOY_1)) == false) & alreadyFired) {
 		alreadyFired = false;
 	}
 
-	VIC.spr_pos[0].x = (short int)x;
+	VIC.spr_pos[0].x = (char)x;
 	VIC.spr_pos[0].y = y;
 
-	VIC.spr_hi_x = (x > 255);
+	VIC.spr_hi_x = (x >> 8);
 
 }
 
 void main() {
-	unsigned char *sprites = (unsigned char*)SPRITE_LOCATION;
-
 	joy_install(joy_static_stddrv);
 
 	clrscr();
+	textcolor(COLOR_BLACK);
+	VIC.spr_bg_prio = 0b00000001;
 	VIC.spr_exp_y  = 0b00000001;
 	VIC.spr_mcolor = 0b00000001;
 	VIC.spr_mcolors[0] = COLOR_LIGHTGREEN;
@@ -61,8 +70,15 @@ void main() {
 	VIC.bgcolor[0] = COLOR_WHITE;
 	VIC.spr_ena = 0b00000001;
 	*(unsigned char*)0x7F8 = SPRITE_LOCATION / 64;
-	for (;;) {
+	memcpy((unsigned char*)SPRITE_LOCATION, shiggy, 63);
+	do {
 		handle_input();
-		memcpy((unsigned*)0x3EC0, shiggy+(63*frame), shiggy_size);
-	}
+
+		gotoxy(0, 0);
+		if (shigCount == 1) {
+			cprintf("%u shig", shigCount);
+		} else {
+			cprintf("%u shigs", shigCount);
+		}
+	} while (true);
 }
